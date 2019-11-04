3338ec7e26a9dbf87184c265453556cb86f5c726
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-04 14:06:16 -0500

    jumped ahead and finished part3 - going back to work on the router problem - is it with maps?

--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,4 +1,51 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class SpeedComparator {
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+public class SpeedComparator implements Comparator<Animal> {
+    @Override
+    public int compare(Animal o1, Animal o2) {
+        if(o1.getSpeed() > o2.getSpeed()) {return -1;}
+        else if (o2.getSpeed() > o1.getSpeed()){return 1;}
+        else {return 0;}
+    }
+
+    @Override
+    public Comparator<Animal> reversed() {
+        return null;
+    }
+
+    @Override
+    public Comparator<Animal> thenComparing(Comparator<? super Animal> other) {
+        return null;
+    }
+
+    @Override
+    public <U> Comparator<Animal> thenComparing(Function<? super Animal, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return null;
+    }
+
+    @Override
+    public <U extends Comparable<? super U>> Comparator<Animal> thenComparing(Function<? super Animal, ? extends U> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Animal> thenComparingInt(ToIntFunction<? super Animal> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Animal> thenComparingLong(ToLongFunction<? super Animal> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<Animal> thenComparingDouble(ToDoubleFunction<? super Animal> keyExtractor) {
+        return null;
+    }
 }
